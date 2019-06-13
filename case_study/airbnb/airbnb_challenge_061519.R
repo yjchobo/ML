99e6b5f662c38cf1ca988a60f3958fb450f4c744
Cho <yocho@amazon.com>
2019-06-12 18:04:25 -0700

    12

--- case_study/airbnb/airbnb_challenge_061519.R
+++ case_study/airbnb/airbnb_challenge_061519.R
@@ -1,9 +1,10 @@
 ###setup
 #setwd("C:/Users/yocho/Documents/young_cho_non_work/airbnb")
-setwd("C:/Users/YC/portfolio/case_study/airbnb")
+#setwd("C:/Users/YC/portfolio/case_study/airbnb")
+setwd("C:/Users/yocho/portfolio/case_study/airbnb")
 
 
-packages.required = c("sqldf","ggplot2","dplyr", "lubridate")
+packages.required = c("sqldf","ggplot2","dplyr", "lubridate","corrplot","rpart","rpart.plot","party","randomforest")
 #install.packages(packages.required)
 lapply(packages.required, library, character.only = TRUE)
 
@@ -142,8 +143,9 @@ t12
 
 p1 <- sqldf('select
               -- target variable
-              (case when c.ts_booking_at_posix is null then 0 else 1 end) as target,
-              
+              (case when c.ts_booking_at_posix is null then 0 else 1 end) as target_cat,
+              (case when c.ts_booking_at_posix is null then null else ts_booking_at_posix - ts_interaction_first_posix end) as target_num,              
+
               -- reservation related attributes
               c.days_duration_requested_stay,
               c.m_guests,
@@ -178,6 +180,106 @@ p1 <- sqldf('select
              ', method = "name__class")
 
 #turn discrete vars to factors before running
+dis_vars = c("dow_checkin","moy_checkin","guest_country","guest_user_stage_first","host_country","room_type","listing_neighborhood", "target_cat")
+p1[dis_vars] <- lapply(p1[dis_vars],as.factor)
+
+
+p1_cl <- p1[, !(colnames(p1) %in% c("target_num"))]
+p1_reg <- p1[, !(colnames(p1) %in% c("target_cat"))]
+
+
+### classification
+
+#logistic regression
+cl_m1 = glm(target_cat ~ .,  family = binomial, data = p1_cl)
+summary(cl_m1)
+
+#decision tree
+#with only a handful of predictors coming out significant from log reg above
+cl_tree <- ctree(target_cat ~ ., data = p1_cl[c("target_cat","days_duration_requested_stay","m_guests",
+                                                "days_from_first_contact_to_first_checkin","dow_checkin",
+                                                "moy_checkin","room_type")])
+plot(cl_tree)
+
+cl_tree2 <- ctree(target_cat ~ ., data = p1_cl[c("target_cat","days_duration_requested_stay","m_guests",
+                                                "days_from_first_contact_to_first_checkin")])
+plot(cl_tree2, gp = gpar(fontsize = 8))
+
+
+
+
+cl_tree2_rp <- rpart(target_cat ~ ., method="class", data = p1_cl[c("target_cat","days_duration_requested_stay","m_guests",
+                                                 "days_from_first_contact_to_first_checkin")])
+
+printcp(cl_tree2_rp) # display the results 
+plotcp(cl_tree2_rp) # visualize cross-validation results 
+summary(cl_tree2_rp) # detailed summary of splits
+
+
+# Random Forest prediction of Kyphosis data
+pl_cl_rf <- p1_cl[, colSums(is.na(p1_cl)) == 0]
+pl_cl_rf <- pl_cl_rf[, !(colnames(pl_cl_rf) %in% c("listing_neighborhood"))]
+p1_cl_rf_fit <- randomForest(target_cat ~ .,  data=pl_cl_rf)
+print(p1_cl_rf_fit) # view results 
+varImpPlot(p1_cl_rf_fit, pch = 20, main = "Importance of Variables")
+
+
+
+# Predict rf
+# set seed
+set.seed(123)
+# create training and test sets
+inTrain <- caret::createDataPartition(y = p1_cl$target_cat, p = 0.8, list = FALSE)
+# subset
+p1_cl_training <- p1_cl[inTrain, ]
+p1_cl_testing <- p1_cl[-inTrain, ]
+
+predictions.rf <- predict(p1_cl_rf_fit, newdata = p1_cl_testing[, !(colnames(p1_cl_testing) %in% c("target_cat"))])
+confusionMatrix(predictions.rf, p1_cl_testing$target_cat)
+
+
+
+
+
+
+
+
+
+#conditional density plots
+layout(matrix(1:3, ncol = 3))
+cdplot(target_cat ~ days_duration_requested_stay, data = p1_cl)
+cdplot(target_cat ~ m_guests, data = p1_cl)
+cdplot(target_cat ~ dow_checkin, data = p1_cl)
+
+dow_checkinSat
+
+
+
+### regression
+reg_m1 = lm(target_num ~ ., data = p1_reg)
+summary(reg_m1)
+
+
+#EDA
+#all
+p2 <- sqldf('select target_cat, count(*) as count_all_inquiry
+              from p1
+              group by target_cat
+             ')
+p2
+
+p1_complete <- p1[complete.cases(p1), ]
+corrplot(cor(p1_complete[,c(2,3,4,5,9,12,15:18)], method = c("spearman")))
+
+
+
+
+
+
+
+
+
+
 
 
 #clustering on hosty and guests and show stats per cluster
